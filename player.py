71a8ab5b6c39debc5aa523c89008a21b864aedc4
pwdkramer <patrick.kramer07@gmail.com>
2023-02-01 14:45:04 -0800

    Added player attributes

---
+++ player.py
@@ -0,0 +1,11 @@
+class Player:
+    def __init__(self, player_class, hit_points, armor_class, strength, constitution, dexterity, intelligence, widsom, charisma):
+        self.player_class = player_class
+        self.hit_points = hit_points
+        self.armor_class = armor_class
+        self.strength = strength
+        self.constitution = constitution
+        self.dexterity = dexterity
+        self.intelligence = intelligence
+        self.widsom = widsom
+        self.charisma = charisma
