a851762f73501ecdfeaccb425289ce43d9a188db
pwdkramer <patrick.kramer07@gmail.com>
2023-02-01 14:31:51 -0800

    first commit

---
+++ menu.py
@@ -0,0 +1,29 @@
+title = """
+ __        __         _    _             
+ \ \      / /__  _ __| | _(_)_ __   __ _ 
+  \ \ /\ / / _ \| '__| |/ / | '_ \ / _` |
+   \ V  V / (_) | |  |   <| | | | | (_| |
+  __\_/\_/_\___/|_|  |_|\_\_|_| |_|\__, |
+ |_   _(_) |_| | ___               |___/ 
+   | | | | __| |/ _ \                    
+   | | | | |_| |  __/                    
+   |_| |_|\__|_|\___|                                                             
+"""
+
+options="""
+1 - Create a Character
+2 - View User Manual
+3 - Exit Game
+"""
+
+def print_menu():
+    print(title)
+    print("\n\n\n\n")
+    print(options)
+
+    menu_input = input()
+    while (menu_input != '1' and menu_input != '2' and menu_input != '3'):
+        print("Please enter the number 1, 2, or 3")
+        menu_input = input()
+
+    return menu_input
